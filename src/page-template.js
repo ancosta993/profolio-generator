698b18c7a04353874cda352096a766bd454993f1
Anthony Dcosta <adcosta2000@gmail.com>
2022-05-23 22:03:31 -0400

    added the user inputs into a HTML page and given them classes

--- src/page-template.js
+++ src/page-template.js
@@ -6,11 +6,57 @@ const generateAbout = aboutText => {
 
    return `
       <section class='my-3' id='about'>
-         <h2 class= 'text-dark bg-primary p-2 display-inline-block">About Me</h2>
-         <p>${about}</p>
+         <h2 class= 'text-dark bg-primary p-2 display-inline-block'>About Me</h2>
+         <p>${aboutText}</p>
       </section>
    `;
 
+};
+
+// create the section for displaying projects
+const generateProjects = projectsArr => {
+
+   return ` 
+      <section class='my-3' id='portfolio'>
+         <h2 class='text-dark bg-primary p-2 display-inline-block'>Work</h2>
+         <div class='flex-row justify-space-between'>
+           ${projectsArr
+               .filter(({feature}) => feature)
+               .map(({name, description, languages, link}) => {
+                  return `
+                     <div class='col-12 mb-2 bg-dark text-light p-3'>
+                        <h3 class='portfolio-item-title text-light'>${name}</h3>
+                        <h5 class='portfolio-languages'>
+                           Built With:
+                           ${languages.join(', ')}
+                        </h5>
+                        <p> ${description}</p>
+                        <a href='${link}' class='btn'><i class='fab fa-github mr-2'></i>View Project on GitHub</a>
+                     </div>
+                  `;
+               })
+               .join('')}
+
+               ${projectsArr
+                  .filter(({feature}) => !feature)
+                  .map(({name, languages, description, link})=> {
+                     return `
+                     <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                        <h3 class="portfolio-item-title text-light">${name}</h3>
+                        <h5 class="portfolio-languages">
+                            Built With:
+                           ${languages.join(', ')}
+                        </h5>
+                        <p>${description}</p>
+                        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+                     </div>
+
+                     `;
+                  })
+                  .join('')}
+         </div>
+      </section>
+   `;
 };
 
 module.exports = templateData => {
@@ -25,6 +71,7 @@ module.exports = templateData => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    </head>
 
    <body>
@@ -39,6 +86,7 @@ module.exports = templateData => {
       </header>
       <main class='container my-5'>
          ${(generateAbout(about))}
+         ${(generateProjects(projects))}
       
       </main>
       <footer class='container text-center py-3'>
